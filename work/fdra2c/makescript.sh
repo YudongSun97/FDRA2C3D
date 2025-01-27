f123fc5a2cedfe4c373a079a8a4ad2c97edbf66e
Camilla Cattania <cattanica@hotmail.it>
2025-01-27 17:15:53 -0500

    Added instructions for engaging cluster.

---
+++ work/fdra2c/makescript.sh
@@ -0,0 +1,21 @@
+module purge
+
+# option 1, using openmpi 1.8.8 and the default gcc (version 4.8.5). It compile
+module load engaging/openmpi/1.8.8
+
+# option 2, using openmpi/3.1.3 which requires gcc 6.3.0. It doesn't compile
+#module load gcc/6.3.0
+#module load openmpi/3.1.3
+
+cd external/util/; 
+make clean; 
+make mode=s
+cd ../hmmvp
+make clean; 
+make mode=s;
+cd ../..
+make clean; 
+make mode=s;
+make mode=s add_bc
+
+ls bin -lh
