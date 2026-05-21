c0517e128809541faf5cce809ee7fc4783e4b35f
Yudong Sun <yudongs@sh03-ln07.stanford.edu>
2026-05-20 19:09:17 -0700

    new_address

--- work/fdra2c/matlab/019_3_faults_25D_vertical_integral/vis2.m
+++ work/fdra2c/matlab/019_3_faults_25D_vertical_integral/vis2.m
@@ -1,9 +1,9 @@
-% Yudong May 7 2025
+% Yudong May 20 2026
 % load the data for the case twosigmas
-addpath(genpath('/home/yudong/FDRA2C_external/fdra2c/'))
+addpath(genpath('/home/users/yudongs/FDRA2C/fdra2c/'))
 %addpath(genpath('/home/yudong/FDRA2C/matlab-misc/'))
-addpath(genpath('/home/yudong/FDRA2C_external_GF/fdra2c/work/fdra2c/matlab/'))
-data_dir = '/nfs/camcat001/yudong/fdra2c/external_GF/test19/';
+addpath(genpath('/home/users/yudongs/FDRA2C/fdra2c/work/fdra2c/matlab/'))
+data_dir = '/home/users/yudongs/fdra2c/test19/';
 sim_name = 'v2Gn';
 load([data_dir sim_name '.mat'])     
 q = fdra2c('qload', s.fs.cf,1);
